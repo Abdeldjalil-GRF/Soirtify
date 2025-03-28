2fc3cfb7bae1e7754fec701a14ddd57f6cab2384
LAGGOUN Khaled <lagkhaled4@gmail.com>
2025-03-28 02:21:29 +0100

    home_page added

---
+++ pages/home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <title>Page Accueil</title>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-white" id="Navbar">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav ms-auto">
+            <a class="nav-item nav-link me-3" href="#">
+                <img src="/assets/images/Navbar/home_icon.png" alt="Home Icon">
+                <span class="sr-only">(current)</span>
+            </a>
+            <a class="nav-item nav-link me-3" href="#">
+                <img src="/assets/images/Navbar/about_us_icon.png" alt="About Us">
+            </a>
+            <a class="nav-item nav-link me-3" href="#">
+                <img src="/assets/images/Navbar/feedback_icon.png" alt="FeedBacks">
+            </a>
+            <a class="nav-item nav-link disabled me-3" href="#">
+                <img src="/assets/images/Navbar/connexion_icon.png">
+            </a>
+            <button type="button" class="btn btn-danger custom-button me-3">connexion</button>
+            <button type="button" class="btn btn-danger custom-button">inscription</button>
+        </div>
+    </div>
+</nav>
+
+    
+</body>
+</html>
+
+<?php
+
+
+
+
+
+?>
